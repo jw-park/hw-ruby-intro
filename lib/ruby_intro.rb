f91cf7792f2dfacd729a2b64e6317514ab30874c
Jungwoo (Jeff) Park <jwp@berkeley.edu>
2019-09-11 01:09:43 -0700

    finished hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,76 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0){|sum, x| sum + x}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+  if arr.length == 0
+    return false
+  end
+
+  i = 0
+  j = arr.length - 1
+  sorted = arr.sort
+  while i < j
+    if sorted[i] + sorted[j] == n
+      return true
+    elsif sorted[i] + sorted[j] < n
+      i += 1
+    else
+      j -= 1
+    end
+  end
+  false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s == '' or s[/[a-zA-Z]+/] != s
+    return false
+  end
+
+  vowels = %w(a e i o u A E I O U)
+  not vowels.include? s[0]
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  length = s.length
+  if s[/[0, 1]+/] != s
+    false
+  elsif s[/[0]+/] == s
+    true
+  elsif length > 2 and s[length - 2, length - 1] == '00'
+    true
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn == '' or price <= 0
+    self.isbn = isbn
+    self.price = price
+  end
+
+  def price_as_string
+    "$#{'%.2f' % self.price}"
+  end
 end
